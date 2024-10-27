6229fcba0e5df7f07c12301d1bfcb58d1826675c
Logan <Logan278024@gmail.com>
2024-10-27 13:28:02 -0500

    Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/LimeLightTest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+
+@TeleOp(name = "LimeLightTest")
+public class LimeLightTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        if (opModeIsActive()) {
+            // Pre-run
+            while (opModeIsActive()) {
+                // OpMode loop
+            }
+        }
+    }
+}
